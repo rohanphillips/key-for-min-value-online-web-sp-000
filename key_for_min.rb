338c02cb9199dac53447c5a91a3ad2a38d22e434
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 16:37:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,6 @@
 def key_for_min_value(name_hash)
   lowestvalue = -1
   lowestkey = ""
-  If name_hash.size != 0
-    name_hash.each do |key, value|
-      if lowestvalue == -1 || value < lowestvalue
-        lowestvalue = value
-        lowestkey = key
-      end
-    end
-    return lowestkey
-  end
+
   return nil
 end
